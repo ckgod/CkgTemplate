7e63580705d41c79106667e86ce8c4a262fcb9c9
ckgod <rhckdrnr123@gmail.com>
2021-05-12 20:38:31 +0900

    Initial commit

---
+++ app/src/main/java/com/ckg/ckgtemplate/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ckg.ckgtemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+// note blue
+// plan red
+// todo yellow
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
